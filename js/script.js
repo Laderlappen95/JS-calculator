e329ea928bb194f90822a7e20153d31484b2646d
Batman <elliot.nelson4@gmail.com>
2024-04-02 14:48:30 +0200

    done

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@
  */
 
 let lcd = null; // displayen
+let sifferArray;
+
 
 
 let memory = 0; // Lagrat/gamlat värdet från display
@@ -21,21 +23,26 @@ function init() {
  */
 function buttonClick(e) {
     let btn = e.target.id; //id för den tangent som tryckte ner
+    //setOperator(btn.substring(0, 1));//VEt ej vad detta är
 
-
-    console.log(btn); //TA BORT SEN
     // kollar om siffertangent är nedtryckt
     if (btn.substring(0, 1) === 'b') {
-         digit = btn.substring(1, 2); // plockar ut siffran från id:et
+        digit = btn.substring(1, 2); // plockar ut siffran från id:et
         addDigit(digit);
-    } else if(btn.substring(0, 1) === 'c') {
-        comma = '.';
-        addComma(comma);
-    }else { // Inte en siffertangent, övriga tangenter.
-
     }
-    console.log(digit);     //TA BORT SEN
-    
+    else if (btn.substring(1, 2) === 'o') {
+        addComma('.');
+    }
+    else if (btn.substring(0, 1) === 'c') {
+        clearLCD();
+    }
+    else if (btn.substring(0, 1) === 'e') {
+        calculate();
+    }
+    else { // Inte en siffertangent, övriga tangenter.
+        setOperator(btn);
+    }
+
 }
 
 
@@ -44,29 +51,64 @@ function buttonClick(e) {
  */
 function addDigit(digit) {
     lcd.value = lcd.value + digit;
+    
 }
 
 /**
  * Lägger till decimaltecken
  */
 function addComma(comma) {
+    let utskrift = lcd.value;
+   if(utskrift.includes('.')){
+    console.log("den fins redan en koma")
+   }
+   else{
     lcd.value = lcd.value + comma;
+   }
+    
 }
 
 /**
  * Sparar operator.
  * +, -, *, /
  */
-function setOperator(operator){
-
-   
+function setOperator(operator) {
+    memory = lcd.value;
+    clearLCD();
+    
+    if(operator === 'sub'){
+        arithmetic = '-';
+    }
+    if(operator === 'div'){
+        arithmetic = '/';
+    }
+    if(operator === 'add'){
+        arithmetic = '+';
+    }
+    if(operator === 'mul'){
+        arithmetic = '*';
+    }
 }
 
 /**
  * Beräknar ovh visar resultatet på displayen.
  */
 function calculate() {
-
+    if(arithmetic === '+'){ 
+       let mem = parseInt(memory);
+       let originValue = parseInt(lcd.value);
+        lcd.value = mem + originValue;
+    }
+    else if(arithmetic === '*'){
+        lcd.value = memory * lcd.value;
+    }
+    else if(arithmetic === '-'){
+        lcd.value = memory - lcd.value;
+    }
+    else if(arithmetic === '/'){
+        lcd.value = memory / lcd.value;
+    }
+    
 }
 
 /** Rensar display */
@@ -76,7 +118,7 @@ function clearLCD() {
 }
 
 /** Rensar allt, reset */
-function memClear(){
+function memClear() {
     memory = 0;
     arithmetic = null;
     clearLCD();
