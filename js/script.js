2727d676298c210bb3443d60e8837d4e855e23db
Elliot Nelson <elliot.nelson4@gmail.com>
2024-05-28 01:26:31 +0200

    klar för inlämning

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@
  */
 
 let lcd = null; // displayen
-let hiddenLCD = null; //gömd display, till för uträckningarna
 
 
 let memory = 0; // Lagrat/gamlat värdet från display
@@ -13,7 +12,6 @@ let arithmetic = null; // Vilken beräkning som skall göras +,-, x eller /
 
 function init() {
     lcd = document.getElementById('lcd');
-
     let keyBoard = document.getElementById('keyBoard')
     keyBoard.onclick = buttonClick;
 }
@@ -23,7 +21,6 @@ function init() {
  */
 function buttonClick(e) {
     let btn = e.target.id; //id för den tangent som tryckte ner
-
 
     // kollar om siffertangent är nedtryckt
     if (btn.substring(0, 1) === 'b') {
@@ -42,17 +39,13 @@ function buttonClick(e) {
     else { // Inte en siffertangent, övriga tangenter.
         setOperator(btn);
     }
-
 }
-
 
 /**
  *  Lägger till siffra på display.
  */
 function addDigit(digit) {
     lcd.value = lcd.value + digit;
-    hiddenLCD.value += digit;
-
 }
 
 /**
@@ -65,13 +58,9 @@ function addComma(comma) {
     }
     else {
         lcd.value = lcd.value + comma;
-        hiddenLCD.value += comma;
     }
 
 }
-// function addOperator(operator){
-//     lcd.value += operator;
-// }
 
 /**
  * Sparar operator.
@@ -83,27 +72,17 @@ function setOperator(operator) {
 
     if (operator === 'sub') {
         arithmetic = '-';
-        addOperator("-");
     }
     if (operator === 'div') {
         arithmetic = '/';
-        addOperator("/");
     }
     if (operator === 'add') {
         arithmetic = '+';
-        addOperator("+");
     }
     if (operator === 'mul') {
         arithmetic = '*';
-        addOperator("*");
     }
 }
-
-function avrundaTillDecimaltal(tal, decimalplatser) {
-    var faktor = Math.pow(10, decimalplatser);
-    return Math.round(tal * faktor) / faktor;
-}
-
 
 /**
  * Beräknar ovh visar resultatet på displayen.
@@ -119,21 +98,15 @@ function calculate() {
         lcd.value = (+memory + +lcd.value) / Math.pow(10, decAmount);
     }
     else if (arithmetic === '-') {
-        lcd.value = +memory - +lcd.value / Math.pow(10, decAmount);
+        lcd.value = (+memory - +lcd.value) / Math.pow(10, decAmount);
     }
-     else if (arithmetic === '*') {
-         lcd.value = +memory * +lcd.value / Math.pow(10, 2 * decAmount);
-     }
-     else if (arithmetic === '/') {
-         lcd.value = +memory / +lcd.value
-     }
- 
-    
+    else if (arithmetic === '*') {
+        lcd.value = +memory * +lcd.value / Math.pow(10, 2 * decAmount);
+    }
+    else if (arithmetic === '/') {
+        lcd.value = +memory / +lcd.value
+    }
 }
-
-
-
-
 
 /** Rensar display */
 function clearLCD() {
